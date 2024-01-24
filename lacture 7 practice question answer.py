8937ef76925d88e2abdb49b9a754c57139753c12
Ameer Huzaifa 138 <155724302+AMEERHUZAIFA138@users.noreply.github.com>
2024-01-24 13:35:15 +0500

    Add files via upload

---
+++ lacture 7 practice question answer.py
@@ -0,0 +1,30 @@
+# lecture 7
+myfavfruits=["apple","mango","grapes"]
+print(myfavfruits)
+myfavfruits.remove("grapes")
+print(myfavfruits)
+myfavfruits.append("banana")
+print(myfavfruits)
+
+
+# Create two lists of numbers
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+
+# Concatenate the two lists
+concatenated_list = list1 + list2
+
+# Display the original lists and the concatenated list
+print("List 1:", list1)
+print("List 2:", list2)
+print("\nConcatenated List:")
+print(concatenated_list)
+
+
+
+daysofweek=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+print(daysofweek)
+print(daysofweek[2])
+
+nameagepairs=[("ameer huzaifa",21),("ameer hamza",50),("ameer hunain",16),("ameer hazeem",19)]
+print(nameagepairs)
